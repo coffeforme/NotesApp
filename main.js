9026ca5786808fd80ceb791236b7af142a7936d1
Camilo Salamanca <casc6798-@hotmail.com>
2022-11-08 20:31:09 -0500

    Notes App

---
+++ main.js
@@ -0,0 +1,33 @@
+window.onload = () => {
+    start();
+}
+
+function start() {
+    const btn_enviar = document.getElementById("btn_enviar")
+    const btn_eliminar = document.getElementById("btn_eliminar")
+    const text_nota = document.getElementById("text_nota")
+
+    btn_enviar.onclick = () => {
+        console.log("Estas clickeando el boton enviar")
+        anadirNota();
+    }
+
+    btn_eliminar.onclick = () => {
+        console.log("Estas clickeando el boton eliminar")
+        // hacer que elimine lo ingresado en el text_nota
+    }
+
+
+}
+
+
+
+
+function anadirNota() {
+    //Funcion que añade la nota a la pagina principal
+    console.log(text_nota.value)
+    const createdNotesDiv = document.getElementById("createdNotes");
+    const newElement = document.createElement("p"); 
+    newElement.textContent = "Soy un parrafo nuevo!";
+
+};
